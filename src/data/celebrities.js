ea2098469f716ed08830a6aae6014a12e5278eed
Ashutosh Choudhary <ashutoshchoudhary.job@gmail.com>
2026-02-25 18:34:16 +0530

    fixed menu items visibility, consistent heading size and consistent spacing

--- src/data/celebrities.js
+++ src/data/celebrities.js
@@ -35,6 +35,30 @@ const celebrities = [
         event: "Tech Summit Keynote",
         image: "/home/carousel-6-min.JPG",
     },
+    {
+        id: 7,
+        name: "Akshay Kumar",
+        event: "Sports Award Ceremony",
+        image: "/home/carousel-7-min.jpg",
+    },
+    {
+        id: 8,
+        name: "Ranveer Singh",
+        event: "Brand Launch Event",
+        image: "/home/carousel-8-min.JPG",
+    },
+    {
+        id: 9,
+        name: "Priyanka Chopra",
+        event: "Global Gala Dinner",
+        image: "/home/carousel-1-min.JPG",
+    },
+    {
+        id: 10,
+        name: "Shah Rukh Khan",
+        event: "Film Premiere Night",
+        image: "/home/carousel-2-min.jpg",
+    },
 ];
 
 export default celebrities;
