968e5220ddc250bf0a9689eafd8b1301a04631cb
singhkevin <hello@kevinsingh.com>
2026-02-27 15:48:25 +0530

    adjusted height for images and video in gallery page

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -7,6 +7,48 @@ import Footer from "../../components/Footer/Footer";
 import ReactLenis from "lenis/react";
 import Transition from "../../components/Transition/Transition";
 
+const VideoCard = ({ vid }) => {
+    const [isPlaying, setIsPlaying] = React.useState(false);
+
+    // Extract YouTube ID to get thumbnail
+    const getYouTubeId = (url) => {
+        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+        const match = url.match(regExp);
+        return (match && match[2].length === 11) ? match[2] : null;
+    };
+
+    const videoId = getYouTubeId(vid.url);
+    const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+
+    return (
+        <div className={`video-item ${isPlaying ? 'is-playing' : ''}`}>
+            <div className="video-wrapper" onClick={() => setIsPlaying(true)}>
+                {!isPlaying ? (
+                    <div className="video-custom-overlay">
+                        <img src={thumbnailUrl} alt={vid.title} className="video-thumb" />
+                        <div className="overlay-content">
+                            <div className="play-button">
+                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M7 6L17 12L7 18V6Z" fill="currentColor" />
+                                </svg>
+                            </div>
+                            <h3>{vid.title}</h3>
+                        </div>
+                    </div>
+                ) : (
+                    <iframe
+                        src={`${vid.url}${vid.url.includes('?') ? '&' : '?'}autoplay=1`}
+                        title={vid.title}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen
+                    ></iframe>
+                )}
+            </div>
+        </div>
+    );
+};
+
 const Gallery = () => {
     return (
         <ReactLenis root>
@@ -44,18 +86,7 @@ const Gallery = () => {
                     </div>
                     <div className="video-grid">
                         {galleryData.videos.map((vid) => (
-                            <div key={vid.id} className="video-item">
-                                <div className="video-wrapper">
-                                    <iframe
-                                        src={vid.url}
-                                        title={vid.title}
-                                        frameBorder="0"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                        allowFullScreen
-                                    ></iframe>
-                                </div>
-                                <h4>{vid.title}</h4>
-                            </div>
+                            <VideoCard key={vid.id} vid={vid} />
                         ))}
                     </div>
                 </section>
